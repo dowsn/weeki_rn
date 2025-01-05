83f75c80d6b6b23b024b28416b29f05c29fe3973
Lukas <lukas@treat.agency>
2025-01-05 18:26:02 +0100

    update

--- src/stacks/YouStack.js
+++ src/stacks/YouStack.js
@@ -34,7 +34,7 @@ function YouStack() {
       }}
     >
       <Stack.Screen
-        name="You"
+        name="YourProfile"
         component={YouScreen}
         options={{
           headerShown: false,
